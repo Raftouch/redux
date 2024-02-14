bdbfa5b70c67079e79a82c2e58c53c6f5a7d7ca7
Raftouch <rafanelkin@gmail.com>
2024-02-14 19:58:30 +0100

    feat :sparkles: created store

---
+++ src/state/store.ts
@@ -0,0 +1,16 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+export const store = configureStore({
+  reducer: {},
+})
+
+// when using TS
+// we can get ReturnType of the function getState to return RootState
+// this type of RootState will help us
+// whenever we need to access RootState using a selector we are going to define this state using ReturnType
+// and will have access to the entire state easily
+export type RootState = ReturnType<typeof store.getState>
+
+// type of our dispatch (send, ship) called AppDispatch
+// when using useDispatch hook
+export type AppDispatch = typeof store.dispatch
