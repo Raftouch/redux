1207a82fb2f1cb74a1411c7ed0c2b7c4307f1cf3
Raftouch <rafanelkin@gmail.com>
2024-02-15 12:40:16 +0100

    clean :bulb: remove useless comments

--- src/state/counter/counterSlice.ts
+++ src/state/counter/counterSlice.ts
@@ -12,28 +12,22 @@ const counterSlice = createSlice({
   name: 'counter',
   initialState,
   reducers: {
-    // always state as a parameter, action is optional
-    // increment: (state, action) => {
     increment: (state) => {
       state.value += 1
     },
     decrement: (state) => {
       state.value -= 1
     },
-    // same -->
-    // incrementByAmount: (state, action: PayloadAction<{value: number}>) => {
-    //   state.value += action.payload.value
     incrementByAmount: (state, action: PayloadAction<number>) => {
       state.value += action.payload
     },
   },
-  // for async functions
+  // async functions -->
   extraReducers: (builder) => {
     builder
       .addCase(incrementAsync.pending, () => {
         console.log('incrementAsync/pending')
       })
-      // when it's done and we have our data fetched
       .addCase(
         incrementAsync.fulfilled,
         (state, action: PayloadAction<number>) => {
@@ -43,10 +37,6 @@ const counterSlice = createSlice({
   },
 })
 
-// all names are automatically saved as counter/increment, counter/decrement etc
-
-// for async functions we do createAsyncThunk first and then reducers
-// for sync functions --> first reducers
 export const incrementAsync = createAsyncThunk(
   'counter/incrementAsync', // name
   async (amount: number) => {
@@ -54,8 +44,6 @@ export const incrementAsync = createAsyncThunk(
     return amount
   }
 )
-// we dispatched our async action with a parameter that we defined on line 52,
-// then we returned an amount that is passed as a payload to line 31 in order to update a state
 
 export const { increment, decrement, incrementByAmount } = counterSlice.actions
 
